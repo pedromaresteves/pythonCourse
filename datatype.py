c3a884489efc38147767fa43a3003b592e977bdd
pmarques <pmarques@idealista.com>
2019-11-11 13:03:00 +0100

    Data type file

---
+++ datatype.py
@@ -0,0 +1,32 @@
+# Strings
+print("Hello World")
+print('Hello world')
+print('''Hello World''')
+print("""Hello World""")
+print(type("Hello World"))
+print("Bye " + "World")
+
+# Integer
+print(30)
+
+# Float
+print(10.5)
+
+# Boolean
+True
+False
+
+# List
+[10, 20, 30, 55]
+["Hello", "Bye", "Adios"]
+[10, "Hello", True, 10.1]
+[]
+
+# Tuples
+(10,20,30,55)
+()
+
+# Dictionaries
+print(type({"name": "Ryan", "apellido": "Ray", "surname": "Poops"}))
+
+print(type(None))
